1be657c5bc235b14ee580baf6b532b765893b3a2
Simonas Kazlauskas <git@kazlauskas.me>
2020-02-04 01:57:13 +0200

    Rename the parallelism flag

--- crate2nix/src/main.rs
+++ crate2nix/src/main.rs
@@ -73,12 +73,12 @@ pub enum Opt {
         crate_hashes: Option<PathBuf>,
 
         #[structopt(
-            short = "j",
-            long = "max-jobs",
+            short = "p",
+            long = "prefetch-parallelism",
             default_value = "4",
-            help = "maximum concurrent fetch jobs"
+            help = "maximum concurrent prefetch jobs"
         )]
-        jobs: usize,
+        prefetch_parallelism: usize,
     },
 
     #[structopt(
@@ -116,7 +116,7 @@ fn main() -> CliResult {
             all_features,
             no_default_features,
             features,
-            jobs,
+            prefetch_parallelism,
         } => {
             let crate_hashes_json = crate_hashes.unwrap_or_else(|| {
                 cargo_toml
@@ -156,7 +156,7 @@ fn main() -> CliResult {
                 nixpkgs_path,
                 crate_hashes_json,
                 other_metadata_options,
-                jobs,
+                prefetch_parallelism,
             };
             let build_info = crate2nix::BuildInfo::for_config(&generate_info, &generate_config)?;
             let nix_string = render::render_build_file(&build_info)?;
